9db7ec941e6ffc7443899054660b23f2e58a6406
Khamitov Timur <ricomen@mail.ru>
2017-04-19 21:46:02 +0500

    add js for mein-nav

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,15 @@
 //jQuery begin
 $(document).ready(function() {
+  //main-nav
+  $(".main-nav__item:has('.main-nav__list-lvl2')").on("mouseenter", function() {
+    var timer = setTimeout(function() {
+      
+    }, 200);
+    $(this).children(".main-nav__list-lvl2").slideDown();
+  });
 
-});
-$(window).load(function() {
-  /* Act on the event */
+
+
+
 });
 //jQuery end
